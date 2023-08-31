53dc452583d2bed95ca71d1eaa9b554de2522d2a
Fernanda <feeh.gomes0110@gmail.com>
2023-08-31 13:59:33 -0300

    change names

--- 1_REACT_ROUTER_DOM/4_navecacao/src/pages/Home.jsx
+++ 1_REACT_ROUTER_DOM/4_navecacao/src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <Link to={"/usuarios"}>Usuarios</Link>
+      <Link to={"/Usuarios"}>Usuarios</Link>
     </div>
   )
 }
--- 1_REACT_ROUTER_DOM/4_navecacao/src/router.jsx
+++ 1_REACT_ROUTER_DOM/4_navecacao/src/router.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import Users from './pages/Users'
+import Usuarios from './pages/Usuarios'
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} /> 
-      <Route path="/users" element={<Users />} /> 
+      <Route path="/Usuarios" element={<Usuarios />} /> 
     </Routes>
   )
 }
